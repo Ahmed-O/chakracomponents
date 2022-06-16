435522763d56a8d4df5c8d5ec9432a3453b77a1a
Ahmed Osman <ahmedosman36610@gmail.com>
2022-06-15 22:28:47 -0700

    worked on shopping cart component

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <>
       <ProdDetails />
       <ColorModeSwitcher />
-      {/* <Simple />;
-      <ShoppingCart /> */}
+      {/* <Simple />; */}
+      <ShoppingCart />
     </>
   );
 }
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {Box, Flex, VStack, HStack, Text, Spacer, Container, Image, useColorModeValue, Heading, Button, Select} from '@chakra-ui/react'
+import React, {useState} from 'react'
+import {Box, Flex, VStack, HStack, Text, Spacer, Container, Image, useColorModeValue, Heading, Button, Select, RadioGroup, Radio} from '@chakra-ui/react'
 
 function ShoppingCart() {
   return (
@@ -10,15 +10,16 @@ function ShoppingCart() {
 
 
 function CartItem(){
+    const [value, setValue] = useState('1')
     return (
-        <HStack>
+        <HStack alignItems={'center'}>
             
             <Image src='https://www.motortrend.com/uploads/sites/5/2020/01/Lamborghini-Huracan-Evo-RWD-front-three-quarters.jpg' w='150px' />
             <VStack>
                 <Text>Lamborghini Huracan</Text>
                 <HStack>
                  <Select placeholder='1' size='md' w='80px' focusBorderColor={useColorModeValue('black','white')}>
-                    <option value='1'>QTY</option>
+                    <option value='1'>1</option>
                     <option value='2'>2</option>
                     <option value='3'>3</option>
                     <option value='4'>4</option>
@@ -35,6 +36,14 @@ function CartItem(){
                
 
             </VStack>
+
+            <RadioGroup onChange={setValue} value={value}>
+                <VStack alignItems='flex-start'>
+                    <Radio value='1'>Standard Shipping</Radio>
+                    <Radio value='2'>Two Day Delivery</Radio>
+                    <Radio value='3'>Order Pickup</Radio>
+                </VStack>
+            </RadioGroup>
             
         </HStack>
     )
