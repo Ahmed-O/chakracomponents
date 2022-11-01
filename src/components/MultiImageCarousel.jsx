ece7c0f1f2db54115d2d39a4df5c782641d8a70b
Ahmed Osman <ahmedosman36610@gmail.com>
2022-10-31 17:09:17 -0700

    Working on carousel functionality

--- src/components/MultiImageCarousel.jsx
+++ src/components/MultiImageCarousel.jsx
@@ -1,19 +1,105 @@
-import React, {useState} from 'react'
-import {Box, Flex, HStack, Image} from '@chakra-ui/react'
+import React, {useState, useEffect, useRef} from 'react'
+import {Box, Flex, HStack, Image, IconButton, Text, useColorModeValue} from '@chakra-ui/react'
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
 
 export default function MultiImageCarousel() {
+  const arrowStyles = {
+    cursor: "pointer",
+    pos: "absolute",
+    top: "75%",
+    w: "auto",
+    //mt: "-22px",
+    p: "15px",
+    color: "white",
+    //fontWeight: "bold",
+    // fontSize: "18px",
+    //transition: "0.6s ease",
+    //borderRadius: "0 3px 3px 0",
+    //userSelect: "none",
+    _hover: {
+      opacity: 0.8,
+      bg: "black",
+    },
+  };
 
-    const cards = [
-        'https://images.unsplash.com/photo-1666845267403-191d298cf198?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80',
-        'https://images.unsplash.com/photo-1666756201003-b46eaf2d5cf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1234&q=80',
-        'https://images.unsplash.com/photo-1666904428342-6975acc1735d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1460&q=80'
-    ]
+  const cards = [
+      'https://images.unsplash.com/photo-1666845267403-191d298cf198?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80',
+      'https://images.unsplash.com/photo-1666756201003-b46eaf2d5cf9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1234&q=80',
+      'https://images.unsplash.com/photo-1666904428342-6975acc1735d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1460&q=80'
+  ]
+
+  const [currIndex, setCurrIndex] = useState(0);
+  const maxCarouselWidth = useRef(0);
+  const carouselElement = useRef(null);
+
+  const movePrev = () => {
+    if(currIndex > 0){
+      setCurrIndex((prevState) => prevState-1);
+    }
+  };
+
+  const moveNext = () => {
+    if(carouselElement.current !== null && carouselElement.current.offsetWidth * currIndex <= maxCarouselWidth.current){
+      setCurrIndex((prevState)=>prevState+1);
+    }
+  };
+
+  const isDisabled = (direction) =>{
+    if (direction === 'prev') {
+      return currIndex <= 0;
+    }
+    if (direction === 'next' && carouselElement.current !== null) {
+      return (
+        carouselElement.current.offsetWidth * currIndex >= maxCarouselWidth.current
+      );
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (carouselElement !== null && carouselElement.current !== null) {
+      carouselElement.current.scrollLeft = carouselElement.current.offsetWidth * currIndex;
+    }
+  }, [currIndex]);
+
+  useEffect(() => {
+    maxCarouselWidth.current = carouselElement.current
+      ? carouselElement.current.scrollWidth - carouselElement.current.offsetWidth
+      : 0;
+  }, []);
+
+ 
   return (
-    <Flex w={"100vw"} alignItems={"center"} justifyContent={"center"}>
-      <Image src={cards[0]}/>
-      <Image src={cards[1]}/>
-      <Image src={cards[2]}/>
+    <Flex w="full" bg={useColorModeValue('gray.100', 'gray.900')} p={10}
+      alignItems="center"
+      justifyContent="center">
+      {/* <Flex>
+
+      </Flex> */}
+      <Flex w={"full"} pos={"relative"} overflow={"hidden"} ref={carouselElement}>
+        {/* IMAGES GO HERE */}
+        <Flex h="400px" w="full" gap={5}>
+          <Image src={cards[0]}/>
+          <Image src={cards[1]}/>
+          <Image src={cards[2]}/>
+          <Image src={cards[0]}/>
+          <Image src={cards[1]}/>
+          <Image src={cards[2]}/>
+          <Image src={cards[0]}/>
+          <Image src={cards[1]}/>
+          <Image src={cards[2]}/>
+        </Flex>   
+      </Flex>
+       <IconButton {...arrowStyles} aria-label="left-arrow" variant={"ghost"} left="10" isDisabled={isDisabled("prev")} onClick={movePrev}>
+          <BiLeftArrowAlt size="40px"/>
+        </IconButton>
+        <IconButton {...arrowStyles} aria-label="left-arrow" variant={"ghost"} right="10" isDisabled={isDisabled("next")} onClick={moveNext}>
+          <BiRightArrowAlt size="40px"/>
+        </IconButton>
+
     </Flex>
+    
   )
 }
+
 
